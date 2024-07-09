32b97c8820860fb7dd94add902eac85b70eeff3f
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-07-09 14:03:58 +0300

    ter

--- assets/CTA/js/main.js
+++ assets/CTA/js/main.js
@@ -79,3 +79,15 @@ jQuery(document).ready(function($){
 		if(section.parents('.no-csstransitions').length > 0 ) animateLayer(section.find('.cd-modal-bg'), 1, false);
 	}
 });
+
+//Google Tag Manager
+(function (w, d, s, l, i) {
+	w[l] = w[l] || [];
+	w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+	var f = d.getElementsByTagName(s)[0],
+	  j = d.createElement(s),
+	  dl = l != "dataLayer" ? "&l=" + l : "";
+	j.async = true;
+	j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+	f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-MV9QQJHV");
